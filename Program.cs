37e5be5f1d9bc1cda4469eeb6ff6139dc7301456
Mark Osborn <mark.osborn@gmail.com>
2025-10-17 20:11:59 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            var configuration = builder.Build();
+
+            var settings1 = new Foo.Settings();
+            configuration.Bind(settings1);
+
+            var settings2 = new Bar.Settings();
+            configuration.Bind(settings2);
+        }
+    }
+}
+
+namespace Foo
+{
+    public class Settings
+    {
+    }
+}
+
+namespace Bar
+{
+    public class Settings
+    {
+    }
+}
